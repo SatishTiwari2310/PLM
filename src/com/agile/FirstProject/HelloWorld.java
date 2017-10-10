4fca3318e3e5ec34f765fc27b693971f3e35a30e
ST00342171 <ST00342171@INBASDLP01802.TechMahindra.com>
2017-10-10 14:09:46 +0530

    rty

--- src/com/agile/FirstProject/HelloWorld.java
+++ src/com/agile/FirstProject/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld implements ICustomAction
 	
 	  //ActionResult actionResult = null;
 	  
-	   return new ActionResult(ActionResult.STRING,"WELCOME User!");
+	   return new ActionResult(ActionResult.STRING,"WELCOME User1!");
 	  
   }
 }
