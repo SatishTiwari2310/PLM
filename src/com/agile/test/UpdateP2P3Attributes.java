7e16f8d587205047c2c988ccebefe4a87042c4ca
ST00342171 <ST00342171@INBASDLP01802.TechMahindra.com>
2017-11-08 16:09:08 +0530

    new Commit(ACP)

---
+++ src/com/agile/test/UpdateP2P3Attributes.java
@@ -0,0 +1,36 @@
+package com.agile.test;
+
+import com.agile.api.APIException;
+import com.agile.api.IAgileSession;
+import com.agile.api.ICell;
+import com.agile.api.IItem;
+import com.agile.api.ItemConstants;
+import com.agile.px.ICustomAction;
+import com.agile.px.ActionResult;
+import com.agile.api.IDataObject;
+import com.agile.api.INode;
+
+public class UpdateP2P3Attributes implements ICustomAction{
+
+	public ActionResult doAction(IAgileSession session, INode actionNode, IDataObject object) {
+		ActionResult outcome = null;
+		try{
+			IItem itemName = (IItem)object;
+			System.out.println("Item Name>> " +itemName);
+			
+			ICell addDesc  = (ICell)itemName.getCell(ItemConstants.ATT_PAGE_TWO_TEXT11);
+			addDesc.setValue("Modified Description");
+			System.out.println(addDesc.getValue());
+			
+			ICell siteInfo = (ICell)itemName.getCell(ItemConstants.ATT_PAGE_THREE_TEXT01);
+			siteInfo.setValue("Modified Site Info");
+			System.out.println(siteInfo.getValue());
+			outcome = new ActionResult(ActionResult.STRING, "PX Completed");
+		}
+		catch(APIException e){
+			e.printStackTrace();
+		}
+		return outcome;
+	}
+
+}
