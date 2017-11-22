9524a7ea9336228194ba1b71eeae18488c9aeb2a
ST00342171 <ST00342171@INBASDLP01802.TechMahindra.com>
2017-11-22 12:02:18 +0530

    added testcase

---
+++ src/com/agile/test/UpdateP2P3AttributesTest.java
@@ -0,0 +1,29 @@
+package com.agile.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UpdateP2P3AttributesTest {
+
+	@Test
+	public void testDoAction() {
+	      int salary =1000;
+	         // assertEquals(500,salary, 0.0);
+	         assertEquals(1000,salary, 0.0);
+	}
+	
+	@Test
+	public void testDoAction1() {
+	      int age=25;			
+	         // assertEquals(15,age, 0.0);
+	         assertEquals(25,age, 0.0);
+	}
+
+	@Test
+	public void testDoAction2() {
+	     String name="Rajeev";		
+	         // assertEquals("Rajeev",name, "Sanjay");
+	         assertEquals("Rajeev",name, "Rajeev");
+	}
+}
